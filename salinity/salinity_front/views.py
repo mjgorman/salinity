e5bca7f0b6c974e5a86f433e531162f979fb2ca6
Michael Gorman <michael@michaeljgorman.com>
2014-12-16 10:19:08 -0800

    Add basic view skeleton, loop logic

--- salinity/salinity_front/views.py
+++ salinity/salinity_front/views.py
@@ -2,6 +2,21 @@
 Web views are setup here. Will display a templated page populated with
 results from checking the redis server
 """
-#from django.shortcuts import render
+from django.http import HttpResponse
+from django.template.loader import get_template
+from django.template import Context
+import logging
 
-# Create your views here.
+def index(request):
+    """
+    The main page, will end up being a timeline like page
+    """
+    logging.info(request)
+    template = get_template('index.html')
+    saltyness = 50
+    roles = ['web', 'lb', 'php', 'app', 'uti', 'queue', 'solr', 'es', 'node', 'nfs', 'sftp', 'rsyslog', 'mmonit']
+    not_stg = ['rsyslog', 'mmonit']
+    envs = ['qa', 'stg', 'prd']
+
+    html = template.render(Context({'saltyness': saltyness, 'roles' : roles, 'not_stg' : not_stg, 'envs' : envs}))
+    return HttpResponse(html)
