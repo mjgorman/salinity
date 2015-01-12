1abbecdab917df34ef610562d63d2d27baacbdb6
Michael Gorman <mgorman@dandb.com>
2015-01-12 13:35:56 -0800

    Remove dandb secific items

--- salinity/salinity/wsgi.py
+++ salinity/salinity/wsgi.py
@@ -10,15 +10,7 @@ import sys
 import os
 import site
 
-#path addition
-site.addsitedir('/opt/salinity/lib/python2.7/site-packages')
-sys.path.append('/opt/salinity/git/salinity/')
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "salinity.settings")
-
-# Activate your virtual env
-activate_env=os.path.expanduser("/opt/salinity/bin/activate_this.py")
-execfile(activate_env, dict(__file__=activate_env))
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
