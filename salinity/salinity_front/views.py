e943cc87e6c230eee417556e30454bee54497669
Neil Rhine <nrhine@dandb.com>
2015-02-13 10:29:42 -0800

    INFRASYS-4913 Adding CI Environment to Salinity

--- salinity/salinity_front/views.py
+++ salinity/salinity_front/views.py
@@ -16,20 +16,21 @@ def index(request):
     logging.info(request)
     server_con = CheckRedis("localhost")
     template = get_template('index.html')
-    roles = ['web', 'lb', 'php', 'app', 'util', 'queue', 'solr', 'es', 'node', 'nfs', 'sftp', 'rsyslog', 'mmonit']
+    roles = {'app':['web', 'lb', 'php', 'app', 'util', 'queue', 'solr', 'es', 'node', 'nfs', 'sftp', 'rsyslog', 'mmonit'], 'ci': ['slave', 'ci']}
     no_stg = ['rsyslog', 'mmonit']
-    envs = ['qa', 'stg', 'prd']
+    envs = {'app':['qa', 'stg', 'prd'], 'ci':['ci']}
     context_dict = {}
     salted = 0
-    for role in roles:
-        for env in envs:
-            context_dict[role + "_" + env] = {'status':server_con.check_failed_role(role, env), 'role':role, 'env':env}
-            if server_con.check_failed_role(role, env) == "GREEN":
-                salted += 1
+    for env_type, env_list in envs.iteritems():
+        for env in env_list:
+            for role in roles[env_type]:
+                context_dict[role + "_" + env] = {'status':server_con.check_failed_role(role, env), 'role':role, 'env':env}
+                if server_con.check_failed_role(role, env) == "GREEN":
+                    salted += 1
 
     sorted_dict = collections.OrderedDict(sorted(context_dict.items()))
     try:
-        saltyness = salted*100/(len(roles*3)-len(no_stg))
+        saltyness = salted*100/((len(roles['app'])*3 + len(roles['ci']))-len(no_stg))
     except ZeroDivisionError:
         saltyness = 0
 
