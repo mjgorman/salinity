c203eacd4881b12db30f63eae608b7508ca2e19b
Michael Gorman <mgorman@dandb.com>
2015-04-22 12:15:30 -0700

    INFRASYS-5410 Use salt instead of redis.keys lookup, fixed tests related to changes

--- salinity/salinity_front/tests.py
+++ salinity/salinity_front/tests.py
@@ -4,6 +4,7 @@ Test will go here, hopefully in the first iteration
 from django.test import TestCase
 from salinity_front.models import CheckRedis
 from mock import MagicMock
+import salt.wheel
 
 class CheckRedisTests(TestCase):
     """
@@ -16,17 +17,18 @@ class CheckRedisTests(TestCase):
         """
         Build the mock of checkredis, allows testing without real redis data
         """
-        self.checkredis.con.keys = MagicMock(name="method")
-        self.checkredis.con.keys.return_value = ['aw1-php70-qa', 'aw1-php80-qa']
         self.checkredis.con.lindex = MagicMock(name="method")
         self.checkredis.con.lindex.return_value = "01"
         self.checkredis.con.get = MagicMock(name="method")
-        self.checkredis.con.get.return_value = "This Highstate went well"
+        self.checkredis.con.get.return_value = "{\"jid\": \"01\", \"return\": {\"service_|-sshd_|-sshd_|-running\": {\"comment\": \"Service sshd is already enabled, and is in the desired state\", \"__run_num__\": 98, \"changes\": {}, \"name\": \"sshd\", \"result\": true}}}"
+        salt.wheel.Wheel.call_func = MagicMock(name="method")
+        salt.wheel.Wheel.call_func.return_value = {"minions": ['aw1-php70-qa', 'aw1-php80-qa']} 
+
     def test_get_server_list(self):
         """
         Find the list of servers (Mocked)
         """
-        self.assertEqual(sorted(self.checkredis.get_server_list("*php*qa")), sorted(['aw1-php70-qa', 'aw1-php80-qa']))
+        self.assertEqual(sorted(self.checkredis.get_server_list("php","qa")), sorted(['aw1-php70-qa', 'aw1-php80-qa']))
     def test_find_last_highstate(self):
         """
         Find the last highstate (Mocked)
